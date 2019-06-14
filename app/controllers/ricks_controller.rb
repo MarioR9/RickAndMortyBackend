b1424e23c8090f4084c87181ea20bd0451bf6b22
Marior9 <mariorodriguezan@gmail.com>
2019-06-14 15:18:20 -0400

    backend to create new rick and new morty

--- app/controllers/ricks_controller.rb
+++ app/controllers/ricks_controller.rb
@@ -3,5 +3,23 @@ class RicksController < ApplicationController
     def index
        render json: Rick.all
     end
+    def login 
+        
+      if Rick.all.find_by(name: params[:name])
+          render json: Rick.all.find_by(name: params[:name])
+      else
+          render json: {message: "not an user"}
+      
+        end
+    end
+
+    def create
+         newRick = Rick.find_or_create_by(name: params[:name], age: params[:age])
+         newMorty = Morty.find_or_create_by(name:params[:mortyName],rick_id: newRick.id)
+         if newRick
+           rick = Rick.all.find_by(name: params[:name])
+        render json: rick
+         end
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :ricks
+  post '/login', to: 'ricks#login'
   resources :morties
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
