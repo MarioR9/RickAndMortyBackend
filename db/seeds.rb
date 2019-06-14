0aa4eea568e52aba3576b445917d405667d3125d
MarioR9 <mariorodriguezan@gmail.com>
2019-06-14 00:47:41 -0400

    new Seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
   rickOne = Rick.find_or_create_by(name:"Mario", age: 78)
+  ricktwo = Rick.find_or_create_by(name:"Luigi", age: 53)
+
 
   mortyOne = Morty.find_or_create_by(name: "Happy Morty", rick_id: 1)
   mortyTwo = Morty.find_or_create_by(name: "Sad Morty", rick_id: 1)
   mortyThree = Morty.find_or_create_by(name: "New Morty", rick_id: 1)
   mortyFour = Morty.find_or_create_by(name: "Evil Morty", rick_id: 1)
+
+  mortyfive = Morty.find_or_create_by(name: "Evil Morty", rick_id: 2)
+  mortysix = Morty.find_or_create_by(name: "Lawyer Morty", rick_id: 2)
+  mortyseven = Morty.find_or_create_by(name: "Morty", rick_id: 2)
+  mortyeight = Morty.find_or_create_by(name: "Smarth Morty", rick_id: 2)
