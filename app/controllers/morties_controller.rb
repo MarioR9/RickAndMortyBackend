02fb4c21eb8692e5a0022a7b384f147d8e004ce5
Marior9 <mariorodriguezan@gmail.com>
2019-06-18 16:18:09 -0400

    create and morties and delete morties from array

--- app/controllers/morties_controller.rb
+++ app/controllers/morties_controller.rb
@@ -3,5 +3,29 @@ class MortiesController < ApplicationController
     def index
         render json: Morty.all
      end
+    def create 
+        currentMorty = Rick.find(params[:rickID]).morties.find{|morty|morty.morty == params[:morty]}
+        if(currentMorty)
+            render json: {message: "Oops wrong Morty"} 
+        else       
+        
+        newMorty = Morty.create(morty: params[:morty], rick_id: params[:rickID])
+        rick = Rick.find(params[:rickID])
+        render json: {user: rick, morties: rick.morties}
+        end
+    end 
+
+    def destroy 
+        
+        mortyID = Rick.find(params[:rickID]).morties.find{|morty|morty.morty == params[:mortyId]}.id
+      
+        Morty.find(mortyID).destroy
+
+        rick = Rick.find(params[:rickID])
+        render json: {user: rick, morties: rick.morties}
+    end
 
 end
+
+
+
